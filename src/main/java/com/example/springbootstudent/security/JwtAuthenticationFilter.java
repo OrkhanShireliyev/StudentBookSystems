9fd3a26e39445017739d73cefd57d2185b506843
Orkhan <orkhan.shireliyev@mail.ru>
2024-12-03 02:56:11 +0400

    modified

--- src/main/java/com/example/springbootstudent/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/springbootstudent/security/JwtAuthenticationFilter.java
@@ -25,10 +25,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
   private final Logger log = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
 
   @Autowired
-  private JwtTokenProvider tokenProvider;
+  private final JwtTokenProvider tokenProvider;
 
   @Autowired
-  private CustomUserDetailsService customUserDetailsService;
+  private final CustomUserDetailsService customUserDetailsService;
+
+  public JwtAuthenticationFilter(JwtTokenProvider tokenProvider, CustomUserDetailsService customUserDetailsService) {
+    this.tokenProvider = tokenProvider;
+    this.customUserDetailsService = customUserDetailsService;
+  }
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
--- src/main/java/com/example/springbootstudent/service/CustomUserDetailsService.java
+++ src/main/java/com/example/springbootstudent/service/CustomUserDetailsService.java
@@ -3,6 +3,7 @@ package com.example.springbootstudent.service;
 import com.example.springbootstudent.model.User;
 import com.example.springbootstudent.repository.UserRepository;
 import com.example.springbootstudent.security.UserPrincipal;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,9 +11,10 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
   @Autowired
-  private UserRepository userRepository;
+  private final UserRepository userRepository;
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
